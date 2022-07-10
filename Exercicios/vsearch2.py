52e5b2aef88f40c10f53f0e47b693327dbfbb96a
Leandro Benitez <benitez.bpm@gmail.com>
2022-07-10 12:17:53 -0300

    subindo vsearch2

--- Exercicios/vsearch2.py
+++ Exercicios/vsearch2.py
@@ -1,6 +1,6 @@
-def search4vowels():
+def search4vowels(word):
+    '''Display any vowels found in an asked-for word.'''
     vowels = set('aeiou')
-    word = input('Provide a word to search for vowels: ')
     found = vowels.intersection(set(word))
     for vowel in found:
         print(vowel)
