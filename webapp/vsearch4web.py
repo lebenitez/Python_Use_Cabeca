5bc678226455e37aeb2dac8a69054209fefa3e7b
Leandro Benitez <benitez.bpm@gmail.com>
2022-07-15 00:19:54 -0300

    Finalizando aplicação vsearch4web

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,21 +1,39 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
+'''
+Aqui utilizei uma função de redirecionamento, para isso tive que importar redirect do módulo flask. 
+Removi para usar uma segunda forma mais performática que é a de adicionar duas rodas a mesma função
+como pode ser visto logo abaixo no código de @app.route('/entry')
+'''
+# @app.route('/')
+# def hello() -> '302':
+#    return redirect('/entry')
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                                the_phrase=phrase,
+                                the_letters=letters,
+                                the_title=title,
+                                the_results=results)
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-@app.route('/search4')
-def do_search() -> str:
-    return str(search4letters('Miguel e Manuela', 'eiru,!'))
-
 @app.route('/entry')
-def entry_page():
-    return render_template('entry.html')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                            the_title='Welcome to search4letters on the Web!')
     
 '''A função debug permite que eu não precise ficar parando e iniciando o serviço. 
- A medida que desenvolvo ele vai tentando já exibir o resultado. '''
+ A medida que desenvolvo ele vai tentando já exibir o resultado. 
+ Para ativar basta colocar debug=True, para inativar remover.'''
 
 app.run(debug=True)
