6bf773ee231761422f8ef410d78ea7dad92a842d
Leandro Benitez <benitez.bpm@gmail.com>
2022-07-16 12:14:49 -0300

    Adicionando log de resultados

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,4 +1,5 @@
 #from flask import Flask, render_template, request, redirect
+from importlib.resources import contents
 from flask import Flask, render_template, request
 from vsearch import search4letters
 
@@ -13,6 +14,10 @@ como pode ser visto logo abaixo no código de @app.route('/entry')
 # def hello() -> '302':
 #    return redirect('/entry')
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -20,6 +25,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                                 the_phrase=phrase,
                                 the_letters=letters,
@@ -31,6 +37,12 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                             the_title='Welcome to search4letters on the Web!')
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return contents
     
 '''A função debug permite que eu não precise ficar parando e iniciando o serviço. 
  A medida que desenvolvo ele vai tentando já exibir o resultado. 
